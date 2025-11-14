4bba2d7b59b8d5637692cd0112286dcc83176327
darrylmorton <darryl.j.morton@gmail.com>
2025-11-14 12:48:00 +0000

    feat: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from 'eslint-plugin-storybook'
-
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -10,19 +9,28 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default defineConfig([
-  globalIgnores(['dist', 'build']),
+  // globalIgnores(['dist', 'build', 'storybook-static']),
+  globalIgnores(['dist', 'build', 'storybook-static']),
   {
+
     files: ['**/*.{ts,tsx}'],
+    ignores: ['dist', 'build', 'storybook-static', '**/*.js'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      // 'plugin:storybook/recommended',
+      // 'plugin:eslint-plugin-storybook/recommended',
+      // storybook
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: { ...globals.browser, ...globals.jest },
     },
+
+    // ignores: ['./storybook/**', './storybook-static/**'],
   },
+  // ...storybook.configs['flat/recommended'],
   eslintConfigPrettier,
 ])
