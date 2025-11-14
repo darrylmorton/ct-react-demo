0f1959b9fe147d8ce854513ea339f309e880de07
darrylmorton <darryl.j.morton@gmail.com>
2025-11-14 12:48:54 +0000

    feat: prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -9,28 +9,20 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default defineConfig([
-  // globalIgnores(['dist', 'build', 'storybook-static']),
   globalIgnores(['dist', 'build', 'storybook-static']),
   {
-
     files: ['**/*.{ts,tsx}'],
-    ignores: ['dist', 'build', 'storybook-static', '**/*.js'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      // 'plugin:storybook/recommended',
-      // 'plugin:eslint-plugin-storybook/recommended',
-      // storybook
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: { ...globals.browser, ...globals.jest },
     },
-
-    // ignores: ['./storybook/**', './storybook-static/**'],
   },
-  // ...storybook.configs['flat/recommended'],
+  ...storybook.configs['flat/recommended'],
   eslintConfigPrettier,
 ])
