f8f9117cde550eb2444d0a6bb79ee0dd793698f8
darrylmorton <darryl.j.morton@gmail.com>
2025-11-16 10:26:04 +0000

    feat: signup form validation

--- src/Components/Forms/SignupForm.tsx
+++ src/Components/Forms/SignupForm.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 // import 'react-app-polyfill/ie11';
-import { Formik, Field, Form, type FormikHelpers } from 'formik'
+import { Formik, Field, Form } from 'formik'
+import * as Yup from 'yup'
 
 interface Values {
   firstName: string
@@ -9,6 +10,27 @@ interface Values {
   password: string
   confirmPassword: string
 }
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(30, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(30, 'Too Long!')
+    .required('Required'),
+  username: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string()
+    .min(8, 'Too Short!')
+    .max(16, 'Too Long!')
+    .required('Required'),
+  confirmPassword: Yup.string()
+    .min(8, 'Too Short!')
+    .max(16, 'Too Long!')
+    .equals([Yup.ref('password')], 'Passwords do not match')
+    .required('Passwords do not match'),
+})
 
 const SignupForm = () => {
   return (
@@ -22,70 +44,87 @@ const SignupForm = () => {
           password: '',
           confirmPassword: '',
         }}
-        onSubmit={(
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>
-        ) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2))
-            setSubmitting(false)
-          }, 500)
+        validationSchema={SignupSchema}
+        onSubmit={(values: Values, actions) => {
+          actions.setSubmitting(false)
+
+          console.log(values)
         }}
       >
-        <FormWrapper>
-          <FormRow>
-            <FormColumn textAlign="left">
-              <label htmlFor="firstName">*First Name:</label>
-              <FormField
-                id="firstName"
-                name="firstName"
-                placeholder="First Name"
-              />
-            </FormColumn>
-            <FormColumn textAlign="left">
-              <label htmlFor="lastName">*Last Name:</label>
-              <FormField
-                id="lastName"
-                name="lastName"
-                placeholder="Last Name"
-              />
-            </FormColumn>
-          </FormRow>
-          <FormRow>
-            <FormColumn textAlign="left">
-              <label htmlFor="username">*Email:</label>
-              <FormField
-                id="username"
-                name="username"
-                placeholder="john@example.com"
-                type="email"
-                width="100%"
-              />
-            </FormColumn>
-          </FormRow>
-          <FormRow>
-            <FormColumn textAlign="left">
-              <label htmlFor="password">*Password:</label>
-              <FormField id="password" name="password" placeholder="Password" />
-            </FormColumn>
-            <FormColumn textAlign="left">
-              <label htmlFor="confirmPassword">*Confirm Password:</label>
-              <FormField
-                id="confirmPassword"
-                name="confirmPassword"
-                placeholder="Confirm Password"
-              />
-            </FormColumn>
-          </FormRow>
-          <FormRow>
-            <FormColumn alignItems="center">
-              <Button type="button">Reset</Button>
-            </FormColumn>
-            <FormColumn alignItems="center">
-              <Button type="submit">Signup</Button>
-            </FormColumn>
-          </FormRow>
-        </FormWrapper>
+        {({ errors, touched }) => (
+          <FormWrapper>
+            <FormRow>
+              <FormColumn textAlign="left">
+                <label htmlFor="firstName">First Name*:</label>
+                <FormField
+                  id="firstName"
+                  name="firstName"
+                  placeholder="First Name"
+                />
+                {errors.firstName && touched.firstName ? (
+                  <FormFieldError>{errors.firstName}</FormFieldError>
+                ) : null}
+              </FormColumn>
+              <FormColumn textAlign="left">
+                <label htmlFor="lastName">Last Name*:</label>
+                <FormField
+                  id="lastName"
+                  name="lastName"
+                  placeholder="Last Name"
+                />
+                {errors.lastName && touched.lastName ? (
+                  <FormFieldError>{errors.lastName}</FormFieldError>
+                ) : null}
+              </FormColumn>
+            </FormRow>
+            <FormRow>
+              <FormColumn textAlign="left">
+                <label htmlFor="username">Email*:</label>
+                <FormField
+                  id="username"
+                  name="username"
+                  placeholder="john@example.com"
+                  type="email"
+                />
+                {errors.username && touched.username ? (
+                  <FormFieldError>{errors.username}</FormFieldError>
+                ) : null}
+              </FormColumn>
+            </FormRow>
+            <FormRow>
+              <FormColumn textAlign="left">
+                <label htmlFor="password">Password*:</label>
+                <FormField
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                {errors.password && touched.password ? (
+                  <FormFieldError>{errors.password}</FormFieldError>
+                ) : null}
+              </FormColumn>
+              <FormColumn textAlign="left">
+                <label htmlFor="confirmPassword">Confirm Password*:</label>
+                <FormField
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                />
+                {errors.confirmPassword && touched.confirmPassword ? (
+                  <FormFieldError>{errors.confirmPassword}</FormFieldError>
+                ) : null}
+              </FormColumn>
+            </FormRow>
+            <FormRow>
+              <FormColumn alignItems="center">
+                <Button type="reset">Reset</Button>
+              </FormColumn>
+              <FormColumn alignItems="center">
+                <Button type="submit">Signup</Button>
+              </FormColumn>
+            </FormRow>
+          </FormWrapper>
+        )}
       </Formik>
     </div>
   )
@@ -133,6 +172,10 @@ const FormField = styled(Field)`
   width: 100%;
   height: 24px;
   font-size: 1rem;
+`
+
+const FormFieldError = styled.div`
+  color: #ff0000;
 `
 
 const Button = styled.button`
