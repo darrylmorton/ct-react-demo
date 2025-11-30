d5ed10eafb1720595f21c606c6a017455c64add2
darrylmorton <darryl.j.morton@gmail.com>
2025-11-30 14:41:41 +0000

    chore: build fix

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,8 +3,11 @@ import { useContext } from 'react'
 import { AuthContext } from '../Pages/AuthProvider.tsx'
 
 export const useAuth = () => {
-  const { user, authToken, login, logout, authenticate } =
-    useContext(AuthContext)
+  const auth = useContext(AuthContext)
+
+  if (!auth) throw new Error('useAuth must be used within AuthProvider')
+
+  const { user, authToken, login, logout, authenticate } = auth
 
   const isAuthorized = () => {
     if (!user) return false
